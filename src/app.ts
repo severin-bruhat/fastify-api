f151eda8106beae648dd006f9fac28103de918ba
Severin Bruhat <sbruhat@gmail.com>
2024-08-04 13:18:27 +0100

    start to add some 'user' logic

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import Fastify from "fastify";
+import userRoutes from "./modules/user/user.route";
 
-const server = Fastify();
+const fastify = Fastify();
 
-server.get('/helloworld', async (req, res) => {
+fastify.get('/helloworld', async (req, res) => {
     return { message: 'Hello World!' }
 })
 
 async function main() {
     try {
-        await server.listen({ port: 3000, host: "0.0.0.0" });
+        
+        fastify.register(userRoutes, {prefix: 'api/users'})
+
+        await fastify.listen({ port: 3000, host: "0.0.0.0" });
         console.log("Server listening at http://localhost:3000");
         
     } catch (error) {
---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+
+import { registerUserHandler } from "./user.controller";
+
+async function userRoutes(server: FastifyInstance) {
+    server.post('/', registerUserHandler);
+}
+
+export default userRoutes;
